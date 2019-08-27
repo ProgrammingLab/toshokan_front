ddd16ec0dd5b2968de740bc4c48bad088405e30c
annin401 <cando85@icloud.com>
2019-08-27 13:58:08 +0900

    feat6つのフォームの作成

---
+++ pages/new/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="field">
+      <label class="label">タイトル</label>
+      <div class="control">
+        <input class="input" type="text">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">筆者</label>
+      <div class="control">
+        <input class="input" type="text">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">出版社</label>
+      <div class="control">
+        <input class="input" type="text">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">出版日</label>
+      <div class="control">
+        <input class="input" type="text">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">説明</label>
+      <div class="control">
+        <textarea class="textarea" />
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">ISBN</label>
+      <div class="control">
+        <input class="input" type="text">
+      </div>
+    </div>
+  </div>
+</template>
