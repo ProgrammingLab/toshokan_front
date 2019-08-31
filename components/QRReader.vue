86a1974ab876fa33fdc52c7c7d1be78c846978a4
kp54 <kangpang65@gmail.com>
2019-09-01 02:13:25 +0900

    fix size and crop

--- components/QRReader.vue
+++ components/QRReader.vue
@@ -18,11 +18,15 @@ export default {
   data: () => ({
     video: null,
     dim: { width: 1280, height: 720 },
+    offset: { x: 0, y: 0 },
     result: null,
   }),
 
   async mounted() {
+    this.$refs.capture.width = this.$refs.capture.scrollWidth;
+    this.$refs.capture.height = this.$refs.capture.scrollHeight;
     this.dim = { width: this.$refs.capture.width, height: this.$refs.capture.height };
+
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment', ...this.dim } })
         .then((stream) => {
@@ -30,6 +34,10 @@ export default {
           this.video.srcObject = stream;
           this.video.play();
           this.capture();
+
+          const settings = stream.getVideoTracks()[0].getSettings();
+          this.offset.x = this.dim.width - settings.width;
+          this.offset.y = this.dim.height - settings.height;
         })
         .catch((err) => {
           // eslint-disable-next-line no-console
@@ -41,12 +49,13 @@ export default {
   methods: {
     capture() {
       const ctx = this.$refs.capture.getContext('2d');
-      ctx.drawImage(this.video, 0, 0);
+      ctx.drawImage(this.video, this.offset.x, this.offset.y);
       const code = jsQR(
         ctx.getImageData(0, 0, this.dim.width, this.dim.height).data,
         this.dim.width, this.dim.height,
       );
       if (code) {
+        this.video.pause();
         ctx.strokeStyle = 'lime';
         ctx.lineWidth = 2;
         ctx.beginPath();
