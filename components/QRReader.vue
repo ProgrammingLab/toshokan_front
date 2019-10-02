dafa5819cbb54dc72890d5c2f295d9845c3ca91b
kp54 <kangpang65@gmail.com>
2019-10-02 10:27:32 +0900

    grab on capture

--- components/QRReader.vue
+++ components/QRReader.vue
@@ -16,6 +16,7 @@ import jsQR from 'jsqr';
 
 export default {
   data: () => ({
+    running: false,
     video: null,
     dim: { width: 1280, height: 720 },
     offset: { x: 0, y: 0 },
@@ -26,39 +27,54 @@ export default {
     this.$refs.capture.width = this.$refs.capture.scrollWidth;
     this.$refs.capture.height = this.$refs.capture.scrollHeight;
     this.dim = { width: this.$refs.capture.width, height: this.$refs.capture.height };
-
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment', ...this.dim } })
-        .then((stream) => {
-          this.video = document.createElement('video');
-          this.video.srcObject = stream;
-
-          const settings = stream.getVideoTracks()[0].getSettings();
-          this.offset.x = Math.floor((this.dim.width - settings.width) / 2);
-          this.offset.y = Math.floor((this.dim.height - settings.height) / 2);
-
-          this.capture();
-        })
-        .catch((err) => {
-          // eslint-disable-next-line no-console
-          console.error(err);
-        });
-    }
   },
 
   methods: {
-    capture() {
+    async capture() {
+      if (!this.running) {
+        this.running = true;
+      }
+
+      if (!this.video) {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment', ...this.dim } });
+            this.video = document.createElement('video');
+            this.video.srcObject = stream;
+
+            const settings = stream.getVideoTracks()[0].getSettings();
+            this.offset.x = Math.floor((this.dim.width - settings.width) / 2);
+            this.offset.y = Math.floor((this.dim.height - settings.height) / 2);
+          } catch (err) {
+            // eslint-disable-next-line no-console
+            console.error(err);
+            return;
+          }
+        }
+      }
       if (this.video.paused) {
         this.video.play();
       }
+
+      window.requestAnimationFrame(this.capture_loop);
+    },
+
+    stop() {
+      this.video.pause();
+      this.running = false;
+    },
+
+    async capture_loop() {
       const ctx = this.$refs.capture.getContext('2d');
       ctx.drawImage(this.video, this.offset.x, this.offset.y);
       const code = jsQR(
         ctx.getImageData(0, 0, this.dim.width, this.dim.height).data,
         this.dim.width, this.dim.height,
       );
+
       if (code) {
         this.video.pause();
+        this.running = false;
 
         // limeで囲う
         ctx.strokeStyle = 'lime';
@@ -73,8 +89,10 @@ export default {
 
         this.result = code.data;
         this.$emit('success', code.data);
-      } else {
-        window.requestAnimationFrame(this.capture);
+      }
+
+      if (this.running) {
+        window.requestAnimationFrame(this.capture_loop);
       }
     },
   },
