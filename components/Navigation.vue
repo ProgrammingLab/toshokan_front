1eade41611768715fe733c11db43fcd83c0b8043
Ryota Egusa <egusa.ryota@gmail.com>
2019-11-01 11:27:12 +0900

    Add login button

---
+++ components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="nav">
+    <button class="button">
+      プロラボアカウントでログイン
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
